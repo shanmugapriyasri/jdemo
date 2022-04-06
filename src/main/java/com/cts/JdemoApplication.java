37aad577ceaf9d625a709aaef03d8c4df67d3a3e
shanmugapriya <shanmugi.shankar@gmail.com>
2022-04-06 06:09:12 +0530

    kk

--- src/main/java/com/cts/JdemoApplication.java
+++ src/main/java/com/cts/JdemoApplication.java
@@ -1,13 +1,16 @@
 package com.cts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdemoApplication {
-
+	static Logger log=LoggerFactory.getLogger(JdemoApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(JdemoApplication.class, args);
+		log.info("Application started..........");
 	}
 
 }
--- src/test/java/com/cts/JdemoApplicationTests.java
+++ src/test/java/com/cts/JdemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cts;
 
+
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @SpringBootTest
 class JdemoApplicationTests {
 
+	Logger log=LoggerFactory.getLogger(JdemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+	log.info("==============> Jenkins test demo startted");
 	}
 
 }
